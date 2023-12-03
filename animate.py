fb79e690e052e9f926180755a10481ff9e170be7
SkyTim-404 <sls22142@gmail.com>
2023-12-04 00:41:20 +0800

    Fix version mismatch

--- animate.py
+++ animate.py
@@ -2,32 +2,49 @@ import torch
 from diffusers import MotionAdapter, AnimateDiffPipeline, DDIMScheduler
 from diffusers.utils import export_to_gif
 
-# Load the motion adapter
-adapter = MotionAdapter.from_pretrained("guoyww/animatediff-motion-adapter-v1-5-2")
-# load SD 1.5 based finetuned model
+adapter = MotionAdapter.from_pretrained(
+    "guoyww/animatediff-motion-adapter-v1-5-2",
+    torch_dtype=torch.float16,
+)
+
 model_id = "SG161222/Realistic_Vision_V5.1_noVAE"
-pipe = AnimateDiffPipeline.from_pretrained(model_id, motion_adapter=adapter)
 scheduler = DDIMScheduler.from_pretrained(
-    model_id, subfolder="scheduler", clip_sample=False, timestep_spacing="linspace", steps_offset=1
+    model_id, 
+    subfolder="scheduler", 
+    clip_sample=False, 
+    timestep_spacing="linspace", 
+    steps_offset=1,
+    torch_dtype=torch.float16,
+)
+
+pipe = AnimateDiffPipeline.from_pretrained(
+    model_id, 
+    motion_adapter=adapter, 
+    torch_dtype=torch.float16, 
+    use_safetensors=True
 )
 pipe.scheduler = scheduler
 
-# enable memory savings
+pipe.enable_attention_slicing()
 pipe.enable_vae_slicing()
+pipe.enable_vae_tiling()
 pipe.enable_model_cpu_offload()
+pipe.enable_xformers_memory_efficient_attention()
 
-output = pipe(
-    prompt=(
-        "masterpiece, bestquality, highlydetailed, ultradetailed, sunset, "
-        "orange sky, warm lighting, fishing boats, ocean waves seagulls, "
-        "rippling water, wharf, silhouette, serene atmosphere, dusk, evening glow, "
-        "golden hour, coastal landscape, seaside scenery"
-    ),
-    negative_prompt="bad quality, worse quality",
-    num_frames=16,
-    guidance_scale=7.5,
-    num_inference_steps=25,
-    generator=torch.Generator("cpu").manual_seed(42),
-)
+prompts = ["masterpiece, bestquality, highlydetailed, ultradetailed, hyper realistic, sunset, "]
+n_prompts = ["bad quality, worse quality"]
+generators = [torch.Generator().manual_seed(42)]
+
+with torch.inference_mode():
+    output = pipe(
+        prompt=prompts,
+        negative_prompt=n_prompts,
+        num_frames=16,
+        guidance_scale=7.5,
+        num_inference_steps=25,
+        generator=generators,
+    )
+
+
 frames = output.frames[0]
 export_to_gif(frames, "animation.gif")
